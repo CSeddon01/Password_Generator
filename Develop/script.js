de47595da80fcf9bb6e62362f71fe98254eaedfb
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-01-09 14:22:58 -0600

    added math.floor

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// Assignment variables for new password
 var numsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log("nums", numsArray.length);
 
@@ -8,8 +8,8 @@ console.log("lowerCase", lowerCaseArray.length);
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 console.log("uppercase", upperCaseArray.length)
 
-var symbols = ["!", "?", "@", "#", "$", "%", "^", "*", "(", ")"];
-console.log("symbols", symbols.length);
+var symbolsArray = ["!", "?", "@", "#", "$", "%", "^", "*", "(", ")"];
+console.log("symbols", symbolsArray.length);
 
 
 
@@ -28,6 +28,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 function generatePassword() {
   // selects password length
   var passLength = parseInt(prompt("How long would you like the password to be? Pick between 8 - 128 characters."));
@@ -39,6 +40,8 @@ if (passLength > 128) {
   alert("Pick a number between 8 and 128.");
     return "";
   }
+
+
 // Confirm other password requirements
   var lowercase = confirm("Would you like to include lowercase characters in the password?")
 if (lowercase) {
@@ -58,23 +61,21 @@ if (specChar) {
 }
   console.log (passLength, lowercase, uppercase, numeric, specChar);
 }
-  alert(passLength, lowercase, uppercase, numeric);
+  // alert(passLength, lowercase, uppercase, numeric);
 
 
-var newPassword(passLength, lowercase, uppercase, numeric) {
-  var length = passLength;
-  var ch
+//Password Generation Function
+newPassword = function (length, masterPassword) {
+  var passwordEl = document.getElementById("display");
+  //Initialize final password as blank string
+  let userPassword = '';
+
+  //Apend random character form masterPassword
+  for (let i = 0; i < length; i++) {
+      userPassword += masterPassword.charAt(Math.floor(Math.random() * masterPassword.length))
+  };
+  passwordEl.innerHTML = userPassword;
 }
-// var i;
-//   for (i = 0; i = passLength; i++ ) {
-//     newPassword.push(low)
-//   }
- 
-
-
-//   var item = items[Math.floor(Math.random()*items.length)];
-// alert(password);
-// }
 
 
 
